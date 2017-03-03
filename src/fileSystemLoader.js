4f43777a83e1b87610f71034059323c191def45b
Mathew Allen <mathew.w.allen@gmail.com>
2017-03-03 16:31:57 -0500

    eslint but more workful

--- src/fileSystemLoader.js
+++ src/fileSystemLoader.js
@@ -8,11 +8,13 @@ import path from 'path';
 function traceKeySorter(first, second) {
   if (first.length < second.length) {
     return first < second.substring(0, first.length) ? -1 : 1;
-  } else if (first.length > second.length) {
-    return first.substring(0, second.length) <= second ? -1 : 1;
-  } else {
-    return first < second ? -1 : 1;
   }
+
+  if (first.length > second.length) {
+    return first.substring(0, second.length) <= second ? -1 : 1;
+  }
+
+  return first < second ? -1 : 1;
 }
 
 export default class FileSystemLoader {
@@ -26,46 +28,53 @@ export default class FileSystemLoader {
 
 
   fetch(_newPath, relativeTo, _trace, initialContents) {
-    let newPath = _newPath.replace(/^["']|["']$/g, ''),
-      trace = _trace || String.fromCharCode(this.importNr++);
-    return new Promise((resolve, reject) => {
-      let relativeDir = path.dirname(relativeTo),
-        rootRelativePath = path.resolve(relativeDir, newPath),
-        fileRelativePath = path.resolve(path.join(this.root, relativeDir), newPath);
+    const newPath = _newPath.replace(/^["']|["']$/g, '');
+    const trace = _trace || String.fromCharCode(this.importNr++);
 
+    return new Promise((resolve, reject) => {
+      const relativeDir = path.dirname(relativeTo);
+      const rootRelativePath = path.resolve(relativeDir, newPath);
+
+      let fileRelativePath = path.resolve(path.join(this.root, relativeDir), newPath);
       // if the path is not relative or absolute, try to resolve it in node_modules
       if (newPath[0] !== '.' && newPath[0] !== '/') {
         try {
           fileRelativePath = require.resolve(newPath);
-        }        catch (e) {}
+        } catch (event) {}
       }
 
-      if (!initialContents) {
-        const tokens = this.tokensByFile[fileRelativePath];
-        if (tokens) { return resolve(tokens); }
-
-        fs.readFile(fileRelativePath, 'utf-8', (err, source) => {
-          if (err && relativeTo && relativeTo !== '/') {
-            resolve([]);
-          } else if (err && (!relativeTo || relativeTo === '/')) {
-            reject(err);
-          }else {
-            this.core.load(source, rootRelativePath, trace, this.fetch.bind(this))
-              .then(({injectableSource, exportTokens}) => {
-              this.sources[trace] = injectableSource;
-              this.tokensByFile[fileRelativePath] = exportTokens;
-              resolve(exportTokens);
-            }, reject);
-          }
-        });
-      }else {
-        this.core.load(initialContents, rootRelativePath, trace, this.fetch.bind(this))
+      if (initialContents) {
+        return this.core.load(initialContents, rootRelativePath, trace, this.fetch.bind(this))
           .then(({injectableSource, exportTokens}) => {
-          this.sources[trace] = injectableSource;
-          this.tokensByFile[fileRelativePath] = exportTokens;
-          resolve(exportTokens);
-        }, reject);
+            this.sources[trace] = injectableSource;
+            this.tokensByFile[fileRelativePath] = exportTokens;
+            return resolve(exportTokens);
+          })
+          .catch(reject);
       }
+
+      const tokens = this.tokensByFile[fileRelativePath];
+      if (tokens) {
+        return resolve(tokens);
+      }
+
+      return fs.readFile(fileRelativePath, 'utf-8', (err, source) => {
+        if (err && relativeTo && relativeTo !== '/') {
+          return resolve([]);
+        }
+
+        if (err && (!relativeTo || relativeTo === '/')) {
+          return reject(err);
+        }
+
+        return this.core.load(source, rootRelativePath, trace, this.fetch.bind(this))
+          .then(({injectableSource, exportTokens}) => {
+            this.sources[trace] = injectableSource;
+            this.tokensByFile[fileRelativePath] = exportTokens;
+            return resolve(exportTokens);
+          })
+          .catch(reject);
+      });
     });
   }
 
