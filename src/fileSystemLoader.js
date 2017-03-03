eddf8f362738df5cee17ec0c1ec499ca38d921fc
Mathew Allen <mathew.w.allen@gmail.com>
2017-03-03 16:25:21 -0500

    autofix fetch

--- src/fileSystemLoader.js
+++ src/fileSystemLoader.js
@@ -13,7 +13,7 @@ function traceKeySorter(first, second) {
   } else {
     return first < second ? -1 : 1;
   }
-};
+}
 
 export default class FileSystemLoader {
   constructor(root, plugins) {
@@ -25,49 +25,48 @@ export default class FileSystemLoader {
   }
 
 
-  fetch( _newPath, relativeTo, _trace, initialContents ) {
-    let newPath = _newPath.replace( /^["']|["']$/g, "" ),
-      trace = _trace || String.fromCharCode( this.importNr++ )
-    return new Promise( ( resolve, reject ) => {
-      let relativeDir = path.dirname( relativeTo ),
-        rootRelativePath = path.resolve( relativeDir, newPath ),
-        fileRelativePath = path.resolve( path.join( this.root, relativeDir ), newPath )
+  fetch(_newPath, relativeTo, _trace, initialContents) {
+    let newPath = _newPath.replace(/^["']|["']$/g, ''),
+      trace = _trace || String.fromCharCode(this.importNr++);
+    return new Promise((resolve, reject) => {
+      let relativeDir = path.dirname(relativeTo),
+        rootRelativePath = path.resolve(relativeDir, newPath),
+        fileRelativePath = path.resolve(path.join(this.root, relativeDir), newPath);
 
       // if the path is not relative or absolute, try to resolve it in node_modules
       if (newPath[0] !== '.' && newPath[0] !== '/') {
         try {
           fileRelativePath = require.resolve(newPath);
-        }
-        catch (e) {}
+        }        catch (e) {}
       }
 
-      if(!initialContents) {
-        const tokens = this.tokensByFile[fileRelativePath]
-        if (tokens) { return resolve(tokens) }
+      if (!initialContents) {
+        const tokens = this.tokensByFile[fileRelativePath];
+        if (tokens) { return resolve(tokens); }
 
-        fs.readFile( fileRelativePath, "utf-8", ( err, source ) => {
-          if ( err && relativeTo && relativeTo !== '/') {
+        fs.readFile(fileRelativePath, 'utf-8', (err, source) => {
+          if (err && relativeTo && relativeTo !== '/') {
             resolve([]);
-          }else if ( err && (!relativeTo || relativeTo === '/')) {
+          } else if (err && (!relativeTo || relativeTo === '/')) {
             reject(err);
-          }else{
-            this.core.load( source, rootRelativePath, trace, this.fetch.bind( this ) )
-            .then( ( { injectableSource, exportTokens } ) => {
-              this.sources[trace] = injectableSource
-              this.tokensByFile[fileRelativePath] = exportTokens
-              resolve( exportTokens )
-            }, reject )
+          }else {
+            this.core.load(source, rootRelativePath, trace, this.fetch.bind(this))
+              .then(({injectableSource, exportTokens}) => {
+              this.sources[trace] = injectableSource;
+              this.tokensByFile[fileRelativePath] = exportTokens;
+              resolve(exportTokens);
+            }, reject);
           }
-        } )
-      }else{
-        this.core.load( initialContents, rootRelativePath, trace, this.fetch.bind(this) )
-        .then( ( { injectableSource, exportTokens } ) => {
-          this.sources[trace] = injectableSource
-          this.tokensByFile[fileRelativePath] = exportTokens
-          resolve( exportTokens )
-        }, reject )
+        });
+      }else {
+        this.core.load(initialContents, rootRelativePath, trace, this.fetch.bind(this))
+          .then(({injectableSource, exportTokens}) => {
+          this.sources[trace] = injectableSource;
+          this.tokensByFile[fileRelativePath] = exportTokens;
+          resolve(exportTokens);
+        }, reject);
       }
-    } )
+    });
   }
 
   get finalSource() {
